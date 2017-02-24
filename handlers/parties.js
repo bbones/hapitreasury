9e2cb88bb62c191436c89c223a1be4f4c35b7166
bbones <boneswilliam@gmail.com>
2017-02-24 17:26:04 +0200

    route tuning

--- handlers/parties.js
+++ handlers/parties.js
@@ -3,15 +3,29 @@ const repo = new Repo()
 const sequelize = repo.sequelize
 const models = require('../models/models')
 
+const JSONAPISerializer = require('jsonapi-serializer').Serializer
+
+const PartySerializer = new JSONAPISerializer('parties',
+  {attributes: ['name', 'created_at', 'updated_at']})
+
 const partyHandlers = {
   postParty: (request, reply) => {
     console.log(request.payload.name)
-    reply(models.Party.create({name: request.payload.name}))
+    models.Party.create({name: request.payload.name}).then((party) => {
+      reply(PartySerializer.serialize(party))
+    })
   },
   getParties: (request, reply) => {
-    reply(models.Party.findAll())
+    models.Party.findAll().then((data) => reply(PartySerializer.serialize(data)))
+  },
+  deleteParty: (request, reply) => {
+    models.Party.findById(request.params.id).then((party) => {
+      party.destroy().then((data) => {
+        console.log(data)
+        reply(PartySerializer.serialize(party))
+      })
+    })
   }
-
 }
 
 module.exports = partyHandlers
